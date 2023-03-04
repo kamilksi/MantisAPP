97a50847a61c3211007f6e8fd7683d9667e02eed
kamilksi <kamilksiazek002@gmail.com>
2023-03-04 23:16:57 +0100

    simple ui

--- lib/presentation/pages/home/home_page.dart
+++ lib/presentation/pages/home/home_page.dart
@@ -1,11 +1,12 @@
 import 'package:av_utils/av_utils.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mantis_app/injectable/injectable.dart';
 import 'package:mantis_app/presentation/pages/home/cubit/home_cubit.dart';
 import 'package:mantis_app/presentation/pages/home/widgets/home_page_body.dart';
-
+import 'package:mantis_app/presentation/utils/theme/app_styles.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -13,8 +14,12 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-      create: (_) => getIt<HomeCubit>()..init(),
-      child: const AVScaffold(canGoBack: false, child: HomePageBody()),
-    );
+        create: (_) => getIt<HomeCubit>()..init(),
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text("Mantis"),
+          ),
+          body: HomePageBody(),
+        ));
   }
 }
