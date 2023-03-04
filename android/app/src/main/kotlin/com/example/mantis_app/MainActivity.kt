d444e514a97e15b28eac3992b5a03ce716fa105d
kamilksi <kamilksiazek002@gmail.com>
2023-03-04 13:36:31 +0100

    commit init

---
+++ android/app/src/main/kotlin/com/example/mantis_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mantis_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: (Text('My App')),
+      ),
+    );
+  }
+}
