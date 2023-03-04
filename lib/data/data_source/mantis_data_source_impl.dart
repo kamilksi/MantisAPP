10ede3572e55f601673d28487b9cf7d165dd626a
kamilksi <kamilksiazek002@gmail.com>
2023-03-04 17:17:57 +0100

    data src impl

---
+++ lib/data/data_source/mantis_data_source_impl.dart
@@ -0,0 +1,15 @@
+import 'package:mantis_app/data/dto/mantis_response_dto.dart';
+import 'package:mantis_app/domain/data_source/mantis_data_source.dart';
+import 'package:retrofit/http.dart';
+import 'package:dio/dio.dart';
+import 'package:mantis_app/networking_config/networking_config.dart';
+part "mantis_data_source_impl.g.dart";
+
+@RestApi()
+abstract class MantisDataSourceImpl implements MantisDataSource {
+  factory MantisDataSourceImpl(Dio dio) = _MantisDataSourceImpl;
+
+  @override
+  @GET(NetworkingEndpoints.getMantis)
+  Future<MantisResponseDto> getMantis();
+}
