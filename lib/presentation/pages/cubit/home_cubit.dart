402c60a95fd482cec407bc365d17ef07c7f77df0
kamilksi <kamilksiazek002@gmail.com>
2023-03-04 17:19:23 +0100

    display api

--- lib/presentation/pages/cubit/home_cubit.dart
+++ lib/presentation/pages/cubit/home_cubit.dart
@@ -1,19 +1,21 @@
 import 'package:bloc/bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:injectable/injectable.dart';
+import 'package:mantis_app/domain/use_cases/get_mantis_use_case.dart';
 import 'home_state.dart';
 
 @injectable
 class HomeCubit extends Cubit<HomeState> {
-  HomeCubit() : super(HomeState.initial());
+  HomeCubit(this._getMantisUseCase) : super(const HomeState.initial());
 
+  final GetMantisUseCase _getMantisUseCase;
   Future<void> init() => _getMantis();
 
   Future<void> _getMantis() async {
-    try {
-      emit(HomeState.success());
-    } catch (e) {
-      emit(HomeState.error());
-    }
+    final result = await _getMantisUseCase();
+    result.fold(
+      (l) => emit(HomeState.error(l)),
+      (r) => emit(HomeState.success(listMantis: r.types)),
+    );
   }
 }
--- lib/presentation/pages/widgets/home_page_body.dart
+++ lib/presentation/pages/widgets/home_page_body.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:mantis_app/domain/entities/response/mantis.dart';
 import 'package:mantis_app/presentation/pages/cubit/home_cubit.dart';
 import 'package:mantis_app/presentation/pages/cubit/home_state.dart';
 
@@ -15,13 +16,23 @@ class HomePageBody extends StatelessWidget {
       builder: (context, state) => state.map(
         initial: (state) => AVProgressIndicator(),
         loading: (state) => AVProgressIndicator(),
-        success: (state) => Center(
-          child: Text("Succes"),
-        ),
+        success: (state) => _buildBody(context, state.listMantis),
         error: (state) => Center(
-          child: Text("Error"),
+          child: Text(state.error.toString()),
         ),
       ),
     );
   }
 }
+
+Widget _buildBody(BuildContext context, List<Mantis> model) => GridView.builder(
+    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+    itemCount: model.length,
+    itemBuilder: (context, index) => Card(
+          child: Column(
+            children: [
+              Image.network(model[index].image!),
+              Text(model[index].name)
+            ],
+          ),
+        ));
